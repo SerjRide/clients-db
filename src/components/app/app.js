b070939cf1455d7333762e2df1073a6b2630c405
SerjRide <quicksquicks6@gmail.com>
2019-02-11 15:34:02 +0300

    first created

---
+++ src/components/app/app.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import './app.css';
+
+export default class App extends Component {
+
+  state = {
+
+  };
+
+
+  render() {
+
+    return (
+      <div className="clientdb-app">
+
+      </div>
+    );
+  }
+}
