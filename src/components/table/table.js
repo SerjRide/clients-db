f997a134a7d5840559c732a3aaa546cf67250cdb
SerjRide <quicksquicks6@gmail.com>
2019-02-12 00:16:12 +0300

    Трансформация данных

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -1,36 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import FilltextService from '../../services/filltext-service';
 
 import './table.css';
 
-const Table = () => {
-  return (
-    <div className="table d-flex">
-        <table className="table">
-          <thead>
-            <tr>
-              <th>id</th>
-              <th>firstName</th>
-              <th>lastName</th>
-              <th>email</th>
-              <th>phone</th>
-              <th>address</th>
-              <th>description</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th>1</th>
-              <th>2</th>
-              <th>3</th>
-              <th>4</th>
-              <th>5</th>
-              <th>6</th>
-              <th>7</th>
-            </tr>
-          </tbody>
-        </table>
-    </div>
-  );
-};
+export default class Table extends Component {
 
-export default Table;
+  filltextService = new FilltextService();
+
+  state = {
+    list: {}
+  }
+
+  constructor() {
+    super();
+    this.updateLiteData();
+  }
+
+  onTableLoaded = (list) => {
+    this.setState({list})
+  };
+
+  updateLiteData() {
+    this.filltextService
+      .getLiteData(30)
+      .then(this.onTableLoaded);
+  };
+
+  render() {
+
+    const { list: {id, firstName, lastName, email,
+          phone, description, street, city, state, zip} } = this.state;
+
+    return (
+      <div className="table d-flex">
+          <table className="table table-hover table-sm">
+            <thead>
+              <tr>
+                <th>id</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Address</th>
+                <th>Description</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>{id}</th>
+                <th>{firstName}</th>
+                <th>{lastName}</th>
+                <th>{email}</th>
+                <th>{phone}</th>
+                <th>Street: {street}<br />
+                    City: {city}<br />
+                    State: {state}<br />
+                    Zip: {zip}</th>
+                <th>{description}</th>
+              </tr>
+            </tbody>
+          </table>
+      </div>
+    );
+  };
+};
